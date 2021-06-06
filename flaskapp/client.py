f65814b37818a6e078f3490ca9c4c5f0584c0c63
testlabgit100 <testlabgit100@gmail.com>
2021-06-07 01:12:15 +0500

    1
    1

--- flaskapp/client.py
+++ flaskapp/client.py
@@ -1,29 +1,34 @@
-# импортируем нужные модули
+import requests
+r = requests.get('http://localhost:5000/')
+print(r.status_code)
+print(r.text)
+r = requests.get('http://localhost:5000/data_to')
+print(r.status_code)
+print(r.text)
+
 import os
 from io import BytesIO
 import base64
-import requests
 
 img_data = None
-# создаем путь к файлу (для кросс-платформенности, например)
-path = os.path.join('./static', 'image0008.png')
-# читаем файл и енкодируем его в строку base64
+path = os.path.join('./static','image0008.png')
 with open(path, 'rb') as fh:
     img_data = fh.read()
     b64 = base64.b64encode(img_data)
-# создаем json словарь, который
-# отправляется на сервер в виде json-строки
-# преобразование делает сама функция отправки запроса post
-jsondata = {'imagebin': b64.decode('utf-8')}
+
+
+jsondata = {'imagebin':b64.decode('utf-8')}
 res = requests.post('http://localhost:5000/apinet', json=jsondata)
 if res.ok:
     print(res.json())
 
-try: 
-    r = requests.get('http://localhost:5000/apixml') 
-    print(r.status_code) 
+try:
+    r = requests.get('http://localhost:5000/apixml')
+    print(r.status_code)
     if(r.status_code!=200):
-        exit(1) 
-        print(r.text)
-except: 
+        exit(1)
+    print(r.text)
+except:
     exit(1)
+
+
