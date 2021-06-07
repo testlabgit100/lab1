69e5f2e4b4d5b42ce96cb95df6960851f2b980b7
testlabgit100 <testlabgit100@gmail.com>
2021-06-07 06:19:47 +0500

    1
    1

--- flaskapp/net.py
+++ flaskapp/net.py
@@ -1,12 +1,12 @@
-import random
+#import random
 
-from tensorflow.keras.layers import Input
-from tensorflow.keras.models import Model
+#from keras.layers import Input
+#from keras.models import Model
 
-from tensorflow.keras.applications.resnet50 import preprocess_input, decode_predictions
-import os
-from PIL import Image
-import numpy as np
+#from keras.applications.resnet50 import preprocess_input, decode_predictions
+#import os
+#from PIL import Image
+#import numpy as np
 
 #from tensorflow.compat.v1 import ConfigProto
 #from tensorflow.compat.v1 import InteractiveSession
@@ -17,40 +17,39 @@ import numpy as np
 #session = InteractiveSession(config=config)
 
 
-height = 224
-width = 224
+#height = 224
+#width = 224
 # копирование изображений в массив нумпай и изменение их размеров
 # попробуйте написать код, который сохраняет соотношение сторон
-nh=224
-nw=224
-ncol=3
+#nh=224
+#nw=224
+#ncol=3
 
-visible2 = Input(shape=(nh,nw,ncol),name = 'imginp')
-resnet = tensorflow.keras.applications.resnet_v2.ResNet50V2(include_top=True, weights='imagenet', input_tensor=visible2, input_shape=None, pooling=None, classes=1000)
-
-
-
-def read_image_files(files_max_count,dir_name):
-    files = [item.name for item in os.scandir(dir_name) if item.is_file()]
-    files_count = files_max_count
+#visible2 = Input(shape=(nh,nw,ncol),name = 'imginp')
+#resnet = keras.applications.resnet_v2.ResNet50V2(include_top=True, weights='imagenet', input_tensor=visible2, input_shape=None, pooling=None, classes=1000)
 
 
-    if(files_max_count>len(files)): # определяем количество файлов не больше max
+
+#def read_image_files(files_max_count,dir_name):
+#    files = [item.name for item in os.scandir(dir_name) if item.is_file()]
+#    files_count = files_max_count
+
+
+#    if(files_max_count>len(files)): # определяем количество файлов не больше max
         files_count = len(files)
-    image_box = [[]]*files_count
-    for file_i in range(files_count): # читаем изображения в список
-        image_box[file_i] = Image.open(dir_name+'/'+files[file_i]) #  / ??
-    return files_count, image_box
+#    image_box = [[]]*files_count
+#   for file_i in range(files_count): # читаем изображения в список
+#        image_box[file_i] = Image.open(dir_name+'/'+files[file_i]) #  / ??
+#    return files_count, image_box
 
 
 # x = preprocess_input(x)
-def getresult(image_box):
-    files_count = len(image_box)
-    images_resized = [[]]*files_count
-    for i in range(files_count):
-        images_resized[i] = np.array(image_box[i].resize((height,width)))/255.0
-    images_resized = np.array(images_resized)
-    out_net = resnet.predict(images_resized)
-    decode = decode_predictions(out_net, top=1)
-    return decode
-
+#def getresult(image_box):
+#    files_count = len(image_box)
+#    images_resized = [[]]*files_count
+#    for i in range(files_count):
+#        images_resized[i] = np.array(image_box[i].resize((height,width)))/255.0
+#    images_resized = np.array(images_resized)
+#    out_net = resnet.predict(images_resized)
+#    decode = decode_predictions(out_net, top=1)
+#    return decode
--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -113,37 +113,37 @@ import json
 
 
 # метод для обработки запроса от пользователя
-@app.route("/apinet", methods=['GET', 'POST'])
-def apinet():
-    neurodic = {}
+#@app.route("/apinet", methods=['GET', 'POST'])
+#def apinet():
+#    neurodic = {}
     # проверяем, что в запросе json данные
-    if request.mimetype == 'application/json':
+#    if request.mimetype == 'application/json':
         # получаем json данные
-        data = request.get_json()
+#        data = request.get_json()
         # берем содержимое по ключу, где хранится файл
         # закодированный строкой base64
         # декодируем строку в массив байт, используя кодировку utf-8
         # первые 128 байт ascii и utf-8 совпадают, потому можно
-        filebytes = data['imagebin'].encode('utf-8')
+#        filebytes = data['imagebin'].encode('utf-8')
         # декодируем массив байт base64 в исходный файл изображение
-        cfile = base64.b64decode(filebytes)
+#        cfile = base64.b64decode(filebytes)
         # чтобы считать изображение как файл из памяти, используем BytesIO
-        img = Image.open(BytesIO(cfile))
-        decode = neuronet.getresult([img])
-        neurodic = {}
-        for elem in decode:
-            neurodic[elem[0][1]] = str(elem[0][2])
-            print(elem)
+#        img = Image.open(BytesIO(cfile))
+#        decode = neuronet.getresult([img])
+#        neurodic = {}
+#        for elem in decode:
+#            neurodic[elem[0][1]] = str(elem[0][2])
+#            print(elem)
         # пример сохранения переданного файла
         # handle = open('./static/f.png','wb')
         # handle.write(cfile)
         # handle.close()
         # преобразуем словарь в json-строку
-        ret = json.dumps(neurodic)
+#        ret = json.dumps(neurodic)
         # готовим ответ пользовател
-        resp = Response(response=ret, status=200, mimetype="application/json")
+#        resp = Response(response=ret, status=200, mimetype="application/json")
     # возвращаем ответ
-    return resp
+#    return resp
 
 
 import lxml.etree as ET
