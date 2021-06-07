eef0edd3222e163f766690cb0963cc96a06633d8
testlabgit100 <testlabgit100@gmail.com>
2021-06-07 05:04:34 +0500

    1
    1

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -68,31 +68,6 @@ from werkzeug.utils import secure_filename
 import os
 # подключаем наш модуль и переименовываем
 # для исключения конфликта имен
-import net as neuronet
-# метод обработки запроса GET и POST от клиента
-@app.route("/net",methods=['GET', 'POST'])
-def net():
-    # создаем объект формы
-    form = NetForm()
-    # обнуляем переменные, передаваемые в форму
-    filename=None
-    neurodic = {}
-    # проверяем нажатие сабмит и валидацию введенных данных
-    if form.validate_on_submit():
-        # файлы с изображениями читаются из каталога static
-        filename = os.path.join('./static', secure_filename(form.upload.data.filename))
-        fcount, fimage = neuronet.read_image_files(10,'./static')
-        # передаем все изображения в каталоге на классификацию
-        # можете изменить немного код и передать только загруженный файл
-        decode = neuronet.getresult(fimage)
-        # записываем в словарь данные классификации
-        for elem in decode:
-            neurodic[elem[0][1]] = elem[0][2]
-        # сохраняем загруженный файл
-        form.upload.data.save(filename)
-    # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
-    # сети, если был нажат сабмит, либо передадим falsy значения
-    return render_template('net.html',form=form,image_name=filename,neurodic=neurodic)
 
 
 from flask import request
