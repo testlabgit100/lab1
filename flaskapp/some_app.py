2112934c0cb8aa6c5499dd6de40ab563790dd66f
testlabgit100 <testlabgit100@gmail.com>
2021-06-07 06:12:40 +0500

    1
    1

--- flaskapp/some_app.py
+++ flaskapp/some_app.py
@@ -30,11 +30,11 @@ def data_to():
 
 
 # модули работы с формами и полями в формах
-from flask_wtf import FlaskForm, RecaptchaField
-from wtforms import StringField, SubmitField, TextAreaField
+#from flask_wtf import FlaskForm, RecaptchaField
+#from wtforms import StringField, SubmitField, TextAreaField
 # модули валидации полей формы
-from wtforms.validators import DataRequired
-from flask_wtf.file import FileField, FileAllowed, FileRequired
+#from wtforms.validators import DataRequired
+#from flask_wtf.file import FileField, FileAllowed, FileRequired
 
 # используем csrf токен, можете генерировать его сами
 SECRET_KEY = 'secret'
@@ -51,21 +51,21 @@ bootstrap = Bootstrap(app)
 
 
 # создаем форму для загрузки файла
-class NetForm(FlaskForm):
+#class NetForm(FlaskForm):
     # поле для введения строки, валидируется наличием данных
     # валидатор проверяет введение данных после нажатия кнопки submit
     # и указывает пользователю ввести данные, если они не введены
     # или неверны
-    openid = StringField('openid', validators=[DataRequired()])
+#    openid = StringField('openid', validators=[DataRequired()])
     # поле загрузки файла
     # здесь валидатор укажет ввести правильные файлы
-    upload = FileField('Load image', validators=[
-        FileRequired(),
-        FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
+#    upload = FileField('Load image', validators=[
+#        FileRequired(),
+#        FileAllowed(['jpg', 'png', 'jpeg'], 'Images only!')])
     # поле формы с capture
-    recaptcha = RecaptchaField()
+#    recaptcha = RecaptchaField()
     # кнопка submit, для пользователя отображена как send
-    submit = SubmitField('send')
+#    submit = SubmitField('send')
 
 
 # функция обработки запросов на адрес 127.0.0.1:5000/net
@@ -79,29 +79,29 @@ import net as neuronet
 
 
 # метод обработки запроса GET и POST от клиента
-@app.route("/net", methods=['GET', 'POST'])
-def net():
+#@app.route("/net", methods=['GET', 'POST'])
+#def net():
     # создаем объект формы
-    form = NetForm()
+#    form = NetForm()
     # обнуляем переменные, передаваемые в форму
-    filename = None
-    neurodic = {}
+#   filename = None
+#   neurodic = {}
     # проверяем нажатие сабмит и валидацию введенных данных
-    if form.validate_on_submit():
+#   if form.validate_on_submit():
         # файлы с изображениями читаются из каталога static
-        filename = os.path.join('./static', secure_filename(form.upload.data.filename))
-        fcount, fimage = neuronet.read_image_files(10, './static')
+#        filename = os.path.join('./static', secure_filename(form.upload.data.filename))
+#        fcount, fimage = neuronet.read_image_files(10, './static')
         # передаем все изображения в каталоге на классификацию
         # можете изменить немного код и передать только загруженный файл
-        decode = neuronet.getresult(fimage)
+#        decode = neuronet.getresult(fimage)
         # записываем в словарь данные классификации
-        for elem in decode:
-            neurodic[elem[0][1]] = elem[0][2]
+#        for elem in decode:
+#            neurodic[elem[0][1]] = elem[0][2]
             # сохраняем загруженный файл
-        form.upload.data.save(filename)
+#        form.upload.data.save(filename)
     # передаем форму в шаблон, так же передаем имя файла и результат работы нейронной
     # сети, если был нажат сабмит, либо передадим falsy значения
-    return render_template('net.html', form=form, image_name=filename, neurodic=neurodic)
+#   return render_template('net.html', form=form, image_name=filename, neurodic=neurodic)
 
 
 from flask import request
